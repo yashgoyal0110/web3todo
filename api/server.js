b1e85d01b225594e28623c7aa00fd40489dd5689
yashgoyal0110 <yg364550@gmail.com>
2025-06-01 18:17:43 +0530

    added fns

--- api/server.js
+++ api/server.js
@@ -10,19 +10,46 @@ const web3 = new Web3(process.env.QUICKNODE_ENDPOINT);
 
 const contract = new web3.eth.Contract(ABI, process.env.CONTRACT_ADDRESS);
 
-const viewTask = async () => {
+app.get("/api/ethereum/view-task/:id", async (req, res) => {
+  const taskId = req.params.id;
   try {
-    const task = await contract.methods.viewTask(1).call();
-    console.log("Task details:", task);
-    return task;
+    const task = await contract.methods.viewTask(taskId).call();
+
+    // Convert BigInts to strings
+    const sanitizedTask = JSON.parse(
+      JSON.stringify(task, (key, value) =>
+        typeof value === "bigint" ? value.toString() : value
+      )
+    );
+
+    return res.json(sanitizedTask);
   } catch (error) {
     console.error("Error fetching task:", error);
-    throw error;
+    return res.status(500).json({ error: "Failed to fetch task" });
   }
-};
-viewTask();
+});
 
-const PORT = process.env.PORT || 3000;
+
+app.get("/api/ethereum/view-all", async (req, res) => {
+  try {
+    const tasks = await contract.methods.allTask().call();
+    return res.json(tasks.map(task => {
+      // Convert BigInts to strings
+      return JSON.parse(
+        JSON.stringify(task, (key, value) =>
+          typeof value === "bigint" ? value.toString() : value
+        )
+      );
+    }
+    ));
+  } catch (error) {
+    console.error("Error fetching task:", error);
+    return res.status(500).json({ error: "Failed to fetch task" });
+  }
+});
+
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
